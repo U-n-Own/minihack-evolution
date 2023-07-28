e39f2c76ef3d35669c3408761f73d328fa4bb888
Angelo Nardone <angelo@MBP-di-Angelo.station>
2023-07-28 18:33:33 +0200

    New implementation for the room task

--- genetic_room/fitness_func.py
+++ genetic_room/fitness_func.py
@@ -7,10 +7,16 @@ import numpy as np
 import math
 from utility_func import movement_dictionary
 
+
+
 def find_distance_grid(x_goal_position: int, y_goal_position: int):
+    """
+    find the distances to the goal of each point in the room.
+    the distance to a point is defined as the minimum number of steps 
+    required starting from that point to get to the goal.
+    """
 
     distance_grid=np.zeros((15,15), int)
-
     for x in range(15):
         for y in range(15):
             distance_grid[x][y]=max(np.abs(x-x_goal_position), np.abs(y-y_goal_position))
@@ -18,6 +24,10 @@ def find_distance_grid(x_goal_position: int, y_goal_position: int):
     return distance_grid
 
 def single_position_score(distance_grid: np.ndarray, x_start: int, y_start: int, movement: int):
+    """
+    define a score for each individual position in the room.
+    if we approach the goal the score will be positive, if we move away negative.
+    """
 
     x_after=x_start+movement_dictionary[movement][0]
     y_after=y_start+movement_dictionary[movement][1]
@@ -34,13 +44,15 @@ def single_position_score(distance_grid: np.ndarray, x_start: int, y_start: int,
 
 
 def fitness_function(movement, distance_grid: np.ndarray):
-
+    """
+    associate rules_grid with a score. 
+    the higher the score the better the rules.
+    """
     score=0
-
     for x in range(15):
         for y in range(15):
             if distance_grid[x][y]!=0:
-                score=score+single_position_score(distance_grid, x, y, movement.rules_grid[x][y])*(1/pow(2, distance_grid[x][y]-1))
+                score+=single_position_score(distance_grid, x, y, movement.rules_grid[x][y])*(1/pow(2, distance_grid[x][y]-1))
     
     return score
 
--- genetic_room/rules.py
+++ genetic_room/rules.py
@@ -10,6 +10,8 @@ import random
 from typing import List
 from utility_func import movement_dictionary, step_dictionary
 
+
+#dictionary used to display movements with arrows
 arrow_dictionary = {
     0 : "\u2191",
     1 : "\u2192",
@@ -26,16 +28,23 @@ class Rule:
     rules_grid: np.ndarray
 
     def __init__(self):
-        self.rules_grid = np.random.randint(8, size=(15,15))
+        """
+        initialize the rule as a matrix such that 
+        each position in the room has a movement associated with it.
+        """
 
+        self.rules_grid = np.random.randint(8, size=(15,15))
         self.rules_grid=self.is_good()
   
 
       
     def is_good(self):
+        """
+        check that the movements do not cause the agent to leave the room, 
+        and if so we change them.
+        """
 
         for x in range(1, 14):
-
             while movement_dictionary[self.rules_grid[0][x]][0]==-1:
                 self.rules_grid[0][x]=random.randint(0,7)
             while movement_dictionary[self.rules_grid[14][x]][0]==1:
@@ -58,24 +67,36 @@ class Rule:
         
 
     def print_rule(self):
+        """
+        print the rule as a matrix of number
+        """
         for x in range(15):
             for y in range(15):
                 print(self.rules_grid[x][y], end=" ")
             print("\n")
     
     def print_rule_movement(self):
+        """
+        print the rule as a matrix of words
+        """
         for x in range(15):
             for y in range(15):
                 print(step_dictionary[self.rules_grid[x][y]], end=" ")
             print("\n")
 
     def print_rule_arrow(self):
+        """
+        print the rule as a matrix of arrow
+        """
         for x in range(15):
             for y in range(15):
                 print(arrow_dictionary[self.rules_grid[x][y]], end=' ')
             print("\n")
 
 def generate_initial_population(size_of_population):
+    """
+    generate a number_of_population of rules that we insert into a list.
+    """
 
     list_of_rules=[]
 
